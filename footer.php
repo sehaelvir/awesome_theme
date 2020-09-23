9e8a5dbfed2580c79750dfc85740ed00214f7557
Elvir Sehic <sehaelvir@gmail.com>
2020-09-23 21:55:57 +0200

    my first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+  <footer>
+    <p>This is a footer</p>
+  </footer> 
+    <?php wp_footer( ); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Awesome Theme</title>
+    <?php wp_head(  ); ?>
+</head>
+<body>
